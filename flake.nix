2aa19c2f4ef5487a96d30e87767677b3ced893b1
grenewode <grenewode@pm.me>
2021-11-28 19:33:33 -0500

    Update nix support, especially create a devenv

--- flake.nix
+++ flake.nix
@@ -1,34 +1,55 @@
 {
-  description = "A very basic flake";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
-
     naersk.url = "github:nmattia/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    mozillapkgs = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+    rust-analyzer = {
+      url = "github:grenewode/rust-analyzer-flake";
+      inputs.naersk.follows = "naersk";
+    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , naersk
-    }:
-      with flake-utils.lib;
-      eachDefaultSystem (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          # Override the version used in naersk
-          naersk-lib = naersk.lib."${system}";
-        in
-        rec {
-          packages.i3switcher = naersk-lib.buildPackage ./.;
-          defaultPackage = packages.i3switcher;
-          apps.i3switcher = mkApp { drv = packages.i3switcher; };
-          defaultApp = apps.i3switcher;
+  outputs = { self, nixpkgs, flake-utils, naersk, mozillapkgs, rust-analyzer }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") { };
+        channel = (mozilla.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          hash = pkgs.lib.fakeHash;
+        });
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = channel.rust;
+          rustc = channel.rust;
+        };
+        defaultPackage = naersk-lib.buildPackage { root = ./.; };
+        defaultPackageName = (builtins.parseDrvName defaultPackage.name).name;
+      in rec {
+        # `nix build`
+        packages = {
+          ${defaultPackageName} = defaultPackage;
+        } // {
+          inherit (channel) rust;
+          inherit (rust-analyzer.packages.${system}) rust-analyzer;
+        };
+        inherit defaultPackage;
 
-          devShell = pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
-        }
-      );
+        # `nix run`
+        apps =
+          builtins.mapAttrs (name: drv: flake-utils.lib.mkApp { inherit drv; });
+        defaultApp = flake-utils.lib.mkApp { drv = defaultPackage; };
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          buildInputs = (with pkgs; [ openssl ]);
+          nativeBuildInputs = [
+            channel.rust
+            channel.rust-src
+            self.packages.${system}.rust-analyzer
+          ] ++ (with pkgs; [ pkg-config ]);
+        };
+      });
 }
