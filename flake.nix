146a75dca32e3d83428025af33809f3bd220cfb9
grenewode <grenewode@pm.me>
2023-06-11 00:16:03 -0400

    Simplify the package so that it continues to work on new nixos installations

--- flake.nix
+++ flake.nix
@@ -1,51 +1,59 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-analyzer = {
-      url = "github:grenewode/rust-analyzer-flake";
-      inputs.naersk.follows = "naersk";
-    };
+
+  nixConfig = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://rust-analyzer-flake.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "rust-analyzer-flake.cachix.org-1:M0/jTcCtgtFl6/aZV4l08+JN9Zf5dHzALWrKmCXeeoU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay, rust-analyzer }:
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-analyzer.url = "github:grenewode/rust-analyzer-flake";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-analyzer }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system; };
+
+        inherit (pkgs) rustc cargo cargo-edit rustPlatform;
+        inherit (pkgs.lib) readFile;
+        inherit (builtins) fromTOML;
+
+        cargoToml = fromTOML (readFile ./Cargo.toml);
+
+        inherit (cargoToml.package) name version;
+        inherit (rust-analyzer.packages.${system}) rust-analyzer-nightly;
+
+        package = rustPlatform.buildRustPackage {
+          inherit name version;
+
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
         };
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
-        };
-        defaultPackage = naersk-lib.buildPackage { root = ./.; };
-        defaultPackageName = (builtins.parseDrvName defaultPackage.name).name;
       in rec {
         # `nix build`
-        packages = {
-          ${defaultPackageName} = defaultPackage;
-        } // {
-          inherit (rust-analyzer.packages.${system}) rust-analyzer;
-        };
-        inherit defaultPackage;
+        packages.${name} = package;
+        packages.default = package;
+        defaultPackage = package;
 
         # `nix run`
-        apps =
-          builtins.mapAttrs (name: drv: flake-utils.lib.mkApp { inherit drv; });
-        defaultApp = flake-utils.lib.mkApp { drv = defaultPackage; };
+        apps.${name} = flake-utils.lib.mkApp { drv = packages.default; };
+        apps.default = apps.${name};
+        defaultApp = apps.default;
 
         # `nix develop`
-        devShell = pkgs.mkShell {
-          buildInputs = (with pkgs; [ openssl ]);
-          nativeBuildInputs = [
-            (rust.override {
-              extensions = [ "rust-src" "clippy-preview" "rustfmt-preview" ];
-            })
-            self.packages.${system}.rust-analyzer
-          ] ++ (with pkgs; [ pkg-config ]);
-        };
+        devShell = (pkgs.mkShell {
+          packages = [ rustc cargo rust-analyzer-nightly cargo-edit ]
+            ++ package.buildInputs;
+        });
       });
 }
